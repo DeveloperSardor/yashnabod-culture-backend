9f75b8879cd0d707092618f42c3d7134a8f10ed4
Sardor <131400267+DeveloperSardor@users.noreply.github.com>
2024-09-05 14:44:21 +0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,17 @@
 export const config = {
   port:  8080,
   local_client_app: 'http://localhost:5173',
-  remote_client_app: 'https://client.yashnabodmadaniyat.uz',
+  remote_client_app: 'http://client.yashnabodmadaniyat.uz',
   local_admin_app: 'http://localhost:5174',
-  remote_admin_app: 'https://admin.yashnabodmadaniyat.uz',
+  remote_admin_app: 'http://admin.yashnabodmadaniyat.uz',
   local_teacher_app: 'http://localhost:5175',
-  remote_teacher_app: 'https://teacher.yashnabodmadaniyat.uz',
+  remote_teacher_app: 'http://teacher.yashnabodmadaniyat.uz',
   allowedDomains: 
           [
-              'https://client.yashnabodmadaniyat.uz',
-              'https://api.yashnabodmadaniyat.uz',
-              'https://admin.yashnabodmadaniyat.uz',
-              'https://teacher.yashnabodmadaniyat.uz'
+              'http://client.yashnabodmadaniyat.uz',
+              'http://api.yashnabodmadaniyat.uz',
+              'http://admin.yashnabodmadaniyat.uz',
+              'http://teacher.yashnabodmadaniyat.uz'
             ]
  
 };
